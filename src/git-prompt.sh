7fb9e438a6c028912ea09f50201e366b8c450c4b
Alex Mercier <pro.alexandre.mercier@gmail.com>
2012-12-27 13:29:43 -0600

    Started implementation

--- src/git-prompt.sh
+++ src/git-prompt.sh
@@ -1,33 +1,77 @@
 #!/usr/bin/env bash
 
-# git_status="?"
-# git_dirty="?"
+_git_info="?"
 
-# # see $git_status
-# function git-status {
-#   if [ "$git_status" == "?" ]; then
-#     git_status="$(git status 2>&1)"
-#   fi
-#   echo -n "$git_status"
-# }
-
-# function is-git-repo {
-#   if [ "$(git-status | grep 'Not a git repository') | wc -l" == "0" ]; then
-#     return 0
-#   else
-#     return 1
-#   fi
-# }
-
-# # see $git_dirty
-# # function git-dirty {
-# #   if is-git-repo && [ "$(git-status | tail -n 1)" != "nothing to commit, working directory clean" ]; then
-# #     echo '*'
-# #   fi
-# # }
-
-# git-ahead
-
-function git-prompt {
-  echo -n ""
+function _git-info {
+	if [ "$_git_info" == "?" ]; then
+		_git_info="$(git rev-parse \
+			--git-dir \
+			--is-inside-git-dir \
+			--is-bare-repository \
+			--is-inside-work-tree \
+			--short HEAD 2>/dev/null)"
+	fi
+	echo "$_git_info"
 }
+
+function is-inside-git-dir {
+	[ "$(_git-info | head -n 2 | tail -n 1)" = "true" ] && return 0 || return 1;
+}
+
+function is-bare-repository {
+	[ "$(_git-info | head -n 3 | tail -n 1)" = "true" ] && return 0 || return 1;
+}
+
+function is-inside-work-tree {
+	[ "$(_git-info | head -n 4 | tail -n 1)" = "true" ] && return 0 || return 1;
+}
+
+function git-sha {
+	_git-info | head -n 5 | tail -n 1
+}
+
+function git-dir {
+  _git-info | head -n 1
+}
+
+function git-branch {
+	is-inside-work-tree || return 1
+	local branch=""
+
+	if [ -d "$(git-dir)/rebase-merge" ]; then
+		# TODO: implement
+	else
+		if [ -d "$(git-dir)/rebase-apply" ]; then
+			# TODO: implement
+		elif [ -f "$(git-dir)/MERGE_HEAD" ]; then
+			# TODO: implement
+		elif [ -f "$(git-dir)/CHERRY_PICK_HEAD" ]; then
+			# TODO: implement
+		elif [ -f "$(git-dir)/REVERT_HEAD" ]; then
+			# TODO: implement
+		elif [ -f "$(git-dir)/BISECT_LOG" ]; then
+			# TODO: implement
+		fi
+
+		if [ -n "$b" ]; then
+			:
+		elif [ -h "$(git-dir)/HEAD" ]; then
+			# symlink symbolic ref
+			b="$(git symbolic-ref HEAD 2>/dev/null)"
+		else
+			local head;
+			read head 2>/dev/null < "$(git-dir)/HEAD"
+			branch="${head#ref: }"
+		fi
+	fi
+
+	if [ -n "$step" ] && [ -n "$total" ]; then
+		# TODO: implement
+	fi
+}
+
+echo -n "is-inside-git-dir ? "; is-inside-git-dir && echo yes || echo no;
+echo -n "is-bare-repository ? "; is-bare-repository && echo yes || echo no;
+echo -n "is-inside-work-tree ? "; is-inside-work-tree && echo yes || echo no;
+echo "git-dir: $(git-dir)"
+echo "git-sha: $(git-sha)"
